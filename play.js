42503dc2f96d2c25ce53737bd4006d2910f02b0b
Ziyu Huang <aprilhuang825@gmail.com>
2022-02-22 22:26:24 +0000

    Create a function called handleUserInput and register it as the data callback handler for stdin.

--- play.js
+++ play.js
@@ -1,6 +1,22 @@
 const net = require("net");
-const connect = require("./client")
+const connect = require("./client");
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  // your code here
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+stdin.on("data", handleUserInput);
 
 console.log("Connecting ...");
 connect();
